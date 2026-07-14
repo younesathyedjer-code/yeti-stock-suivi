e893fccc3d1425cea67b56a56c5fdc523452ac83
younes ath yedjer <younes.ath.yedjer@gmail.com>
2026-07-14 13:53:51 +0100

    Update test-1.0

--- tools/core/services/BuildManager.cjs
+++ tools/core/services/BuildManager.cjs
@@ -12,21 +12,120 @@ class BuildManager {
 
 
 
-    execute(plan) {
+    run(command) {
 
-
-        if (!plan.build)
-            return;
-
+        console.log("\n> " + command);
 
 
         execSync(
-            "npm run build",
+            command,
             {
                 cwd: this.config.projectRoot,
                 stdio: "inherit"
             }
         );
+
+    }
+
+
+
+
+
+    execute(plan) {
+
+
+        console.log("\nBUILD MANAGER");
+
+
+
+        if (plan.build) {
+
+
+            console.log(
+                "Build web demandé."
+            );
+
+
+            this.run(
+                "npm run build"
+            );
+
+
+        }
+        else {
+
+
+            console.log(
+                "Build web ignoré."
+            );
+
+
+        }
+
+
+
+
+
+        if (plan.capacitorSync) {
+
+
+            console.log(
+                "Synchronisation Capacitor demandée."
+            );
+
+
+            this.run(
+                "npx cap sync android"
+            );
+
+
+        }
+        else {
+
+
+            console.log(
+                "Capacitor sync ignoré."
+            );
+
+
+        }
+
+
+
+
+
+        if (plan.androidRelease) {
+
+
+            console.log(
+                "Generation APK Release demandée."
+            );
+
+
+            this.run(
+                "android\\gradlew.bat assembleRelease"
+            );
+
+
+        }
+        else {
+
+
+            console.log(
+                "APK Release ignorée."
+            );
+
+
+        }
+
+
+
+        console.log(
+            "\nBUILD MANAGER terminé."
+        );
+
+
+        return true;
 
 
     }
